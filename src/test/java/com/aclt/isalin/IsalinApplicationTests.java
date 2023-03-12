88a417463a38e8c99f8381df4516ce74f760393f
ACLT <me@acltabontabon.com>
2023-03-13 05:00:51 +0800

    Initial commit

---
+++ src/test/java/com/aclt/isalin/IsalinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aclt.isalin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsalinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
