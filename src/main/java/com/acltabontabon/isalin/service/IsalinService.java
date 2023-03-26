9b1e00c38e51dfcfb9c5cb2413af56504d359d3d
ACLT <me@acltabontabon.com>
2023-03-26 17:01:39 +0800

    Restore  isalinService

--- src/main/java/com/acltabontabon/isalin/service/IsalinService.java
+++ src/main/java/com/acltabontabon/isalin/service/IsalinService.java
@@ -10,43 +10,28 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 @Slf4j
 @RequiredArgsConstructor
 public class IsalinService {
 
     private final IsalinProperties isalinProperties;
 
-    private TranslationServiceClient translationServiceClient;
-
-    @SneakyThrows
-    @PostConstruct
-    private void init() {
-        translationServiceClient = TranslationServiceClient.create();
-    }
-
     @SneakyThrows
     public String translate(String input, Language source, Language target) {
-        TranslateTextRequest request = TranslateTextRequest.newBuilder()
-                .setParent(LocationName.of(isalinProperties.getProjectId(), "global").toString())
-                .setSourceLanguageCode(source.getCode())
-                .setTargetLanguageCode(target.getCode())
-                .addContents(input)
-                .build();
+        try (TranslationServiceClient translationServiceClient = TranslationServiceClient.create()) {
+            TranslateTextRequest request = TranslateTextRequest.newBuilder()
+                    .setParent(LocationName.of(isalinProperties.getProjectId(), "global").toString())
+                    .setSourceLanguageCode(source.getCode())
+                    .setTargetLanguageCode(target.getCode())
+                    .addContents(input)
+                    .build();
 
-        TranslateTextResponse response = translationServiceClient.translateText(request);
+            TranslateTextResponse response = translationServiceClient.translateText(request);
 
-        if (!response.getTranslationsList().isEmpty()) {
-            return response.getTranslationsList().stream().findFirst().get().getTranslatedText();
+            if (!response.getTranslationsList().isEmpty()) {
+                return response.getTranslationsList().stream().findFirst().get().getTranslatedText();
+            }
+            return "";
         }
-
-        return "";
-    }
-
-    @PreDestroy
-    private void cleanup() {
-        translationServiceClient.close();
     }
 }
