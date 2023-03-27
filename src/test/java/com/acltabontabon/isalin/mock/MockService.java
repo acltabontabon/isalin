6d64f23bbe5d0f6afc0909958fa744af1c2e58a1
ACLT <me@acltabontabon.com>
2023-03-28 03:12:56 +0800

    Support document translation

--- src/test/java/com/acltabontabon/isalin/mock/MockService.java
+++ src/test/java/com/acltabontabon/isalin/mock/MockService.java
@@ -4,6 +4,8 @@ import com.acltabontabon.isalin.Language;
 import com.acltabontabon.isalin.annotation.Translate;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+
 @Service
 public class MockService {
 
